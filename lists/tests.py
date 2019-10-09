be3ce75df69573d611b38b5b1e4e3a73179cf683
Kevin Sidhu <sidhu.kevin@gmail.com>
2019-10-09 08:55:43 -0400

    add HttpRequest import to test

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -7,3 +9,5 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+    
+    
